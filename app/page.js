e5158c64b645f5287d650504c15c397c430ddbf0
aloshvp <aloshvp12@gmail.com>
2026-01-02 20:13:02 +0530

    why choose us

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 
 import Banner from "@components/home/Banner";
 import HomeGallery from "@components/home/HomeGallery";
+import WhyChooseUs from "@components/home/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <HomeGallery />
+      <WhyChooseUs />
     </>
   );
 }

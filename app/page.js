a9ff04d3c20b466a425a15ee48a468b946852e1f
aloshvp <aloshvp12@gmail.com>
2026-01-02 19:49:37 +0530

    home gallery

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 
 
 import Banner from "@components/home/Banner";
+import HomeGallery from "@components/home/HomeGallery";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <HomeGallery />
     </>
   );
 }

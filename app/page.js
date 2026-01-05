5a9090472125f52f30d69eb18f1eb5037070b1b2
aloshvp <aloshvp12@gmail.com>
2026-01-05 20:19:47 +0530

    what we do

--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ import Banner from "@components/home/Banner";
 import HomeGallery from "@components/home/HomeGallery";
 import WhyChooseUs from "@components/home/WhyChooseUs";
 import PostTension from "@components/home/PostTension";
+import WhatWeDo from "@components/home/WhatWeDo";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <WhatWeDo />
       <HomeGallery />
       <WhyChooseUs />
       <PostTension />
